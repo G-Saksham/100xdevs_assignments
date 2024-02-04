3e561c3418423e78eaca1364f473ec18e00beacb
Saksham Gupta <f20180674@pilani.bits-pilani.ac.in>
2024-02-04 16:13:03 +0530

    practise useMemo hook

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -7,12 +7,18 @@ export const Assignment3 = () => {
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
+        { name: 'Tomato', value: 100 },
         { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(()=>{
+        let totalValue = 0;
+        items.map(item => totalValue += item.value)
+        return totalValue;
+    }, [items]);
+
     // Your code ends here
     return (
         <div>
